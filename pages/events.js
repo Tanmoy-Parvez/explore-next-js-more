15dba8fa4e77a858e1ec394a126f9372f5914b4f
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2022-10-04 16:34:38 +0600

    some changes added

--- pages/events.js
+++ pages/events.js
@@ -3,7 +3,21 @@ const EventsList = ({ eventList }) => {
     console.log(eventList);
     return (
         <div>
-            <h1 className="text-3xl text-center text-white">You have { eventList.length} events to enjoy.</h1>
+            <h1 className="text-3xl text-center text-white">You have {eventList.length} events to enjoy.</h1>
+            {
+                eventList.map(event => <div key={event.id}>
+                    <div className="card w-96 bg-base-100 shadow-xl mt-5 mx-auto">
+                        <div className="card-body">
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-square btn-sm">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                                </button>
+                            </div>
+                            <p>We are using cookies for no reason.</p>
+                        </div>
+                    </div>
+                </div>)
+            }
         </div>
     );
 };
