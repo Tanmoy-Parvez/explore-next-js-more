935d9b5c34b17a857c968f3c269fd08afe7a1b41
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2022-10-10 13:59:28 +0600

    adding user dashboard

--- pages/dashboard-swr.js
+++ pages/dashboard-swr.js
@@ -16,15 +16,37 @@ const DashboardSWR = () => {
     if (!data) return "No data found"
 
     return (
-        <div className="text-white text-center">
+        <div className="text-white">
             <Head>
                 <title>Next.JS-User/Dashboard</title>
             </Head>
             <h1 className="text-2xl">User Dashboard</h1>
-            <p>Total Users: {data.users}</p>
-            <p>Total Posts: {data.posts}</p>
-            <p>Total Likes: {data.likes}</p>
-            <p>Total Comments: {data.comments}</p>
+            <ul className="menu bg-base-100 w-56 p-2 rounded-box">
+                <li>
+                    <a>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
+                        <p>Total Users: {data.users}</p>
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                        <p>Total Users: {data.posts}</p>
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" /></svg>
+                        <p>Total Users: {data.likes}</p>
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                        <p>Total Users: {data.comments}</p>
+                    </a>
+                </li>
+            </ul>
         </div>
     );
 };
