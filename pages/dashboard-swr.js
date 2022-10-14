4bfa6ed182a5807499665afe3655c93c5ad13d6c
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2022-10-14 13:49:27 +0600

    using swr for data fetching

--- pages/dashboard-swr.js
+++ pages/dashboard-swr.js
@@ -1,9 +1,22 @@
 import useSWR from "swr";
 
+const fetcher = async () => {
+    const response = await fetch('http://localhost:5000/dashboard');
+    const data = await response.json();
+    return data;
+}
+
+
 const DashboardSWR = () => {
+    const { data, error } = useSWR('dashboard', fetcher);
+    console.log(data);
+
+    if (error) return "Some error occurred"
+    if (!data) return "No data found"
+
     return (
         <div>
-            
+
         </div>
     );
 };
