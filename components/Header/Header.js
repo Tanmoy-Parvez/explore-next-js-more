08331a09255f7c860efde3e428da63c455c75543
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2022-10-18 01:41:31 +0600

    adding navbar

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,8 +1,26 @@
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className="navbar bg-base-100">
+            <div className="flex-1">
+                <a className="btn btn-ghost normal-case text-xl">Next.JS</a>
+            </div>
+            <div className="flex-none">
+                <ul className="menu menu-horizontal p-0">
+                    <li><a>Blog</a></li>
+                    <li tabIndex={0}>
+                        <a>
+                            Products
+                            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
+                        </a>
+                        <ul className="p-2 bg-base-100">
+                            <li><a>Product 1</a></li>
+                            <li><a>Product 2</a></li>
+                        </ul>
+                    </li>
+                    <li><a>About</a></li>
+                </ul>
+            </div>
         </div>
     );
 };
