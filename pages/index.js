8e055d2141b94615b35e1cd1a831fc2ce8350197
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2022-10-18 02:54:50 +0600

    adding banner

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Banner from "../components/Banner/Banner";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 
@@ -6,6 +7,7 @@ const Home = () => {
   return (
     <div>
       <Header />
+      <Banner/>
       <Footer/>
     </div>
   );
