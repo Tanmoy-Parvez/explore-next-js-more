a2c376728df10876bf1e6df8e894f4707ddc509c
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2022-10-18 11:40:14 +0600

    using next head component

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Banner from "../components/Banner/Banner";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
@@ -6,6 +7,11 @@ import Header from "../components/Header/Header";
 const Home = () => {
   return (
     <div>
+      <Head>
+        <title>
+          Next JS Project
+        </title>
+      </Head>
       <Header />
       <Banner/>
       <Footer/>
