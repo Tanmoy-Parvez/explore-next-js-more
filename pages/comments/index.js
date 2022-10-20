d8a1391f56db7153007a20a4bfc98763c33a1a73
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2022-10-20 23:47:45 +0600

    display comments

--- pages/comments/index.js
+++ pages/comments/index.js
@@ -1,46 +1,23 @@
 
 const CommentsList = ({ comments }) => {
-    console.log(comments);
+    
     return (
         <div className="grid grid-cols-3 gap-4 mx-auto container mt-5">
-            <div className="card card-compact w-96 bg-base-100 shadow-xl">
-                <figure>
-                    <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
-                </figure>
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
+            {
+                comments.map(comment => <div key={comment.id} className="card card-compact w-96 bg-base-100 shadow-xl">
+                    <figure>
+                        <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title">{comment?.body.length>100 ? comment.body.slice(0,100) + '...':comment.body}</h2>
+                        <h3>{comment?.name}</h3>
+                        <h3>{comment?.email}</h3>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Buy Now</button>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div className="card card-compact w-96 bg-base-100 shadow-xl">
-                <figure>
-                    <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
-                </figure>
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
-                </div>
-            </div>
-            <div className="card card-compact w-96 bg-base-100 shadow-xl">
-                <figure>
-                    <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
-                </figure>
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
-                </div>
-            </div>
-
-            
+                </div>)
+            }
         </div>
     );
 };
@@ -48,7 +25,7 @@ const CommentsList = ({ comments }) => {
 export default CommentsList;
 
 export const getStaticProps = async() => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/comments?_limit=100");
+    const res = await fetch("https://jsonplaceholder.typicode.com/comments?_limit=15");
     const data = await res.json();
 
     return {
