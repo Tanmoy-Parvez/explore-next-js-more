318f318b2c631a0a928ca3888a9836d9e4be17d6
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2022-10-20 23:37:08 +0600

    comments component created

---
+++ pages/comments/index.js
@@ -0,0 +1,48 @@
+
+const CommentsList = () => {
+    return (
+        <div className="grid grid-cols-3 gap-4 mx-auto container mt-5">
+            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                <figure>
+                    <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                <figure>
+                    <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                <figure>
+                    <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+
+            
+        </div>
+    );
+};
+
+export default CommentsList;
+
