f5a72fa683250626b0fbb7b814b225e96e047ee7
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2022-10-23 10:46:04 +0600

    some changes added

---
+++ pages/products/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const ProductList = () => {
+    return (
+        <div>
+            <Head>
+                <title>Next.JS-Product List</title>
+            </Head>
+            
+        </div>
+    );
+};
+
+export default ProductList;
