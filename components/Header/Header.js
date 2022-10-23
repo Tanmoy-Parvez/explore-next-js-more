c510c5b201ed8ae1e69d0c0884435a5abf1bcdf7
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2022-10-23 10:37:05 +0600

    some changes added

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -30,25 +30,6 @@ const Header = () => {
                             <a>Blog</a>
                         </Link>
                     </li>
-                    <li tabIndex={0}>
-                        <a>
-                            Products
-                            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-                        </a>
-                        <ul className="p-2 bg-base-100">
-                            <li>
-                                <Link href="/products/1">
-                                    <a>Product-1</a>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href="/products/2">
-                                    <a>Product-2</a>
-                                </Link>
-                            </li>
-
-                        </ul>
-                    </li>
                     <li>
                         <Link href="/about">
                             <a>About</a>
