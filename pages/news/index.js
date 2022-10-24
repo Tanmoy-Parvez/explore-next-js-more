a3d2a14352e51940bb03ffe8f05f77487215de1d
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2022-10-24 21:50:34 +0600

    using getServerSideProps

--- pages/news/index.js
+++ pages/news/index.js
@@ -8,3 +8,14 @@ const NewsList = () => {
 };
 
 export default NewsList;
+
+export const getServerSideProps = async () => {
+    const response = await fetch("http://localhost:5000/news");
+    const data = await response.json();
+    console.log(data);
+    return {
+        props: {
+            
+        }
+    }
+}
