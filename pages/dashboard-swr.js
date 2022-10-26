f267d4a590acbe8f15760f3b7c3706845cb5f227
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2022-10-26 14:13:53 +0600

    some changes added

--- pages/dashboard-swr.js
+++ pages/dashboard-swr.js
@@ -28,6 +28,12 @@ const DashboardSWR = () => {
                         <p>Total Users: {data.users}</p>
                     </a>
                 </li>
+                <li>
+                    <a>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                        <p>Total Users: {data.posts}</p>
+                    </a>
+                </li>
                 
             </ul>
         </div>
